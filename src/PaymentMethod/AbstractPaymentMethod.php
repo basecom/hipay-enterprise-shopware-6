f6c5f6cee8a475af449f2365be04a58a07716f29
Adam BRECHET <abrechet@hipay.com>
2024-10-29 09:24:29 +0100

    Added Klarna payment method

--- src/PaymentMethod/AbstractPaymentMethod.php
+++ src/PaymentMethod/AbstractPaymentMethod.php
@@ -227,6 +227,11 @@ abstract class AbstractPaymentMethod implements AsynchronousPaymentHandlerInterf
         return null;
     }
 
+    public static function requiresBasket(): bool
+    {
+        return false;
+    }
+
     /**
      * Generate the config for the payment method.
      */
@@ -280,6 +285,10 @@ abstract class AbstractPaymentMethod implements AsynchronousPaymentHandlerInterf
         array $payload,
         AsyncPaymentTransactionStruct $transaction
     ): OrderRequest {
+        if (isset($payload['payment_product'])) {
+            $orderRequest->payment_product = $payload['payment_product'];
+        }
+
         return $orderRequest;
     }
 
@@ -391,12 +400,14 @@ abstract class AbstractPaymentMethod implements AsynchronousPaymentHandlerInterf
     ): OrderRequest {
         $isCaptureAuto = $this->config->isCaptureAuto();
         $order = $transaction->getOrder();
-
         $operationId = Uuid::randomHex();
         $orderRequest->orderid = $order->getOrderNumber().'-'.dechex(crc32($transaction->getOrderTransaction()->getId()));
         $orderRequest->operation = $isCaptureAuto ? 'Sale' : 'Authorization';
         $orderRequest->description = $this->generateDescription($order->getLineItems(), 255, '...');
-        // $orderRequest->basket = $this->generateBasket($transaction->getOrder());
+
+        if (static::requiresBasket()) {
+            $orderRequest->basket = $this->generateBasket($order);
+        }
 
         // Amounts data
         if ($order->getCurrency()) {
@@ -457,7 +468,6 @@ abstract class AbstractPaymentMethod implements AsynchronousPaymentHandlerInterf
         return strlen($description) <= $maxLength ? $description : substr($description, 0, $maxLength).$trailing;
     }
 
-    /*
     private function generateBasket(OrderEntity $order): array
     {
         $listType = [
@@ -466,6 +476,7 @@ abstract class AbstractPaymentMethod implements AsynchronousPaymentHandlerInterf
 
         $basket = [];
         foreach ($order->getLineItems() as $lineItem) {
+            $tax_rate = $lineItem->getPrice()->getTaxRules()->first() ? $lineItem->getPrice()->getTaxRules()->first()->getTaxRate() : null;
             $basket[] = [
                 'product_reference' => $lineItem->getPayload()['productNumber'],
                 'name' => $lineItem->getLabel(),
@@ -473,14 +484,13 @@ abstract class AbstractPaymentMethod implements AsynchronousPaymentHandlerInterf
                 'quantity' => $lineItem->getQuantity(),
                 'unit_price' => $lineItem->getUnitPrice(),
                 'discount' => 0,
-                // 'tax_rate' => $lineItem->getPrice()->getCalculatedTaxes(),
+                'tax_rate' => $tax_rate,
                 'total_amount' => $lineItem->getTotalPrice(),
             ];
         }
 
         return $basket;
     }
-    */
 
     /**
      * Retreive billing informations.
