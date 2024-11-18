bc3af49db5593a66714436aa5c8058ffb8a6c802
Claudiu Lazar <lazar.claudiu19@gmail.com>
2024-11-18 13:31:14 +0200

    Audit 3.2.4 - Update to use Attributes instead of annotations

--- src/Route/HipayCardToken/HipayCardTokenRoute.php
+++ src/Route/HipayCardToken/HipayCardTokenRoute.php
@@ -8,11 +8,9 @@ use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-/**
- * @Route(defaults={"_routeScope"={"store-api"}})
- */
+#[Route(defaults: ['_routeScope' => 'store-api'])]
 class HipayCardTokenRoute extends AbstactHipayCardTokenRoute
 {
     /**
@@ -27,9 +25,7 @@ class HipayCardTokenRoute extends AbstactHipayCardTokenRoute
         throw new DecorationPatternException(self::class);
     }
 
-    /**
-     * @Route("/store-api/customer/get-card-token", name="store-api.card-token.get", methods={"GET", "POST"})
-     */
+    #[Route('/store-api/customer/get-card-token', name: 'store-api.card-token.get', methods: ['GET', 'POST'])]
     public function load(Criteria $criteria, SalesChannelContext $context): ?HipayCardTokenRouteResponse
     {
         $criteria = (new Criteria())
