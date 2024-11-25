e2bfdbae74cd6c3eb16e22c7b1a4243334d04c24
Claudiu Lazar <lazar.claudiu19@gmail.com>
2024-11-25 14:29:10 +0200

    Override refund modal for order

---
+++ src/Resources/app/administration/src/override/sw-order-state-change-modal-attach-documents/index.js
@@ -0,0 +1,8 @@
+import template from './sw-order-state-change-modal-attach-documents.html.twig';
+
+/**
+ * Inject credit cards selector
+ */
+Shopware.Component.override('sw-order-state-change-modal-attach-documents', {
+  template
+});
